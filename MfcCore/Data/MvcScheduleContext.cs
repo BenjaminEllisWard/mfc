d243cc2744fae068e9340471435bb4f1c50ca55e
Ben Ward <benjamin.e.ward@outlook.com>
2018-10-31 19:13:37 -0400

    Create EFDb for daily schedules

---
+++ MfcCore/Data/MvcScheduleContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MfcCore.Models
+{
+    public class MvcScheduleContext : DbContext
+    {
+        public MvcScheduleContext (DbContextOptions<MvcScheduleContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MfcCore.Models.Schedule> Schedule { get; set; }
+    }
+}
---
+++ MfcCore/Migrations/20181031230356_Initial.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MfcCore.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Schedule",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Location = table.Column<string>(nullable: true),
+                    Date = table.Column<DateTime>(nullable: false),
+                    Open = table.Column<DateTime>(nullable: false),
+                    Close = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Schedule", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Schedule");
+        }
+    }
+}
