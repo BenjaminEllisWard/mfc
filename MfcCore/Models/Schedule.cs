ccd1c7101922aaf04ddd01c38e4521b7bffa52f2
Ben Ward <benjamin.e.ward@outlook.com>
2018-10-31 18:56:36 -0400

    Add Schedule.cs model

---
+++ MfcCore/Models/Schedule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MfcCore.Models
+{
+    public class Schedule
+    {
+        public int ID { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime Open { get; set; }
+        public DateTime Close { get; set; }
+    }
+}
